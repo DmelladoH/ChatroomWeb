3ab64e99195fdb34dbe92bbec619ce74da2eb13a
DmelladoH <mellado.m2@gmail.com>
2022-07-04 11:18:00 +0200

    messages test schema

---
+++ server/tests/message.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const { server } = require('../index')
+
+beforeEach(async () => {
+
+})
+
+describe('GET / getting', () => {
+  test('all messages of a room when the user is authorized', async () => {})
+
+  test('an error when the user is not subscribed to the room', async () => {})
+
+  test('an error when no authorized user', async () => {})
+
+  test('an error when the room id is invalid', async () => {})
+})
+
+describe('POST / post', () => {
+  test('post a message to a room when the user is authorized', async () => {})
+
+  test('an error when the user is not subscribed to the room', async () => {})
+
+  test('an error when the message body is empty', async () => {})
+
+  test('an error when there is no message body', async () => {})
+
+  test('an error when no authorized user', async () => {})
+})
+
+afterAll(() => {
+  mongoose.disconnect()
+  server.close()
+})

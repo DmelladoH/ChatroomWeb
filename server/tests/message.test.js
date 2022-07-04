b478661797d013691f4258cc519b0dc3f35915fc
DmelladoH <mellado.m2@gmail.com>
2022-07-04 11:30:20 +0200

    new messages test schemas

--- server/tests/message.test.js
+++ server/tests/message.test.js
@@ -18,6 +18,10 @@ describe('GET / getting', () => {
 describe('POST / post', () => {
   test('post a message to a room when the user is authorized', async () => {})
 
+  test('the messages with page breaks are saved properly', async () => {})
+
+  test('the messages with special chacacters are saved properly (", \', \\)', async () => {})
+
   test('an error when the user is not subscribed to the room', async () => {})
 
   test('an error when the message body is empty', async () => {})

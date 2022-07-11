e5bd186c97186d64c5792eb98496e264aebd5954
DmelladoH <mellado.m2@gmail.com>
2022-07-11 20:47:32 +0200

    Top nav prototype

---
+++ client/src/components/TopNav/index.jsx
@@ -0,0 +1,19 @@
+import './TopNav.css'
+
+export default function TopNav () {
+  return (
+    <header>
+      <div className='container'>
+        <div className='logo-container'>
+          <h1>logo</h1>
+        </div>
+        <div className='searchBar-container'>
+          buscador
+        </div>
+        <div className='user-container'>
+          user
+        </div>
+      </div>
+    </header>
+  )
+}

01780f3a8fb5662d619f7c1e1167d7f3d35b92a3
DmelladoH <mellado.m2@gmail.com>
2022-07-12 17:47:41 +0200

    remove navs from the login and register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,26 @@ import { Route, Routes, BrowserRouter, Outlet, Navigate } from 'react-router-dom
 import { useLocation } from 'react-router'
 
 import Login from './pages/Login'
+import Chatroom from './pages/Chatroom'
 import Home from './pages/Home'
 import Register from './pages/Register'
 
 import useUser from './hooks/useUser'
 import { UserContextProvider } from './context/UserContextProvider'
+import RoomsNav from './components/RoomsNav'
+import TopNav from './components/TopNav'
 
 const PortectedRoutes = () => {
   const { isLogged } = useUser()
   const location = useLocation()
   return isLogged
-    ? <Outlet />
+    ? (
+      <>
+        <TopNav />
+        <RoomsNav />
+        <Outlet />
+      </>
+      )
     : <Navigate to='/login' replace state={{ from: location }} />
 }
 
@@ -25,6 +34,7 @@ function App () {
           <Routes>
             <Route element={<PortectedRoutes />}>
               <Route path='/' element={<Home />} />
+              <Route path='/:room' element={<Chatroom />} />
             </Route>
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />

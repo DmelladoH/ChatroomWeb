99c8f9d7cda7ec2246663fb145c0323fbb986dc5
DmelladoH <daniel.mellado@cafler.com>
2022-08-09 20:34:05 +0200

    fix getUser service

--- client/src/server/UserService.jsx
+++ client/src/server/UserService.jsx
@@ -1,15 +1,30 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/users'
 
+let token = null
+let config = {
+  headers: {
+    Authorization: token
+  }
+}
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`
+  config = {
+    headers: {
+      Authorization: token
+    }
+  }
+}
+
 const createAccountService = async (userData) => {
   const { data } = await axios.post(baseUrl, userData)
   return data
 }
 
 const getUser = async (id) => {
-  const { data } = await axios.get(`${baseUrl}/${id}`)
-  console.log({ data })
+  const { data } = await axios.get(`${baseUrl}/${id}`, config)
   return data
 }
 
-export default { createAccountService, getUser }
+export default { createAccountService, getUser, setToken }

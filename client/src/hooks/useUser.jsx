a3c3dd5d34780217b0c5671c72a5494e92d04937
DmelladoH <daniel.mellado@cafler.com>
2022-08-09 20:34:24 +0200

    add useUser

--- client/src/hooks/useUser.jsx
+++ client/src/hooks/useUser.jsx
@@ -1,36 +1,26 @@
-import { useCallback, useContext } from 'react'
-
+import UserService from '../server/UserService'
 import Context from '../context/UserContextProvider'
-import loginService from '../server/loginService'
-// import UserService from '../server/UserService'
 
-function useUser () {
-  const { jwt, setJwt, user, setUser } = useContext(Context)
+import { useEffect, useState, useContext } from 'react'
 
-  const login = useCallback(({ userName, password }) => {
-    loginService({ userName, password })
+function useUser ({ id }) {
+  const [user, setUser] = useState({})
+  const { jwt } = useContext(Context)
+
+  useEffect(() => {
+    UserService.setToken(jwt)
+    UserService.getUser(id)
       .then(data => {
-        const jwt = data.token
-        const id = data.id
-
-        console.log(id)
-        setUser(id)
-
-        window.sessionStorage.setItem('jwt', jwt)
-        window.sessionStorage.setItem('user', id)
-        setJwt(jwt)
-      }).catch(error => {
-        window.sessionStorage.removeItem('jwt')
-        console.error(error)
+        setUser({
+          id: data.id,
+          name: data.name,
+          usename: data.userName,
+          rooms: data.room
+        })
       })
-  }, [setJwt, setUser])
+  }, [id])
 
-  const logout = useCallback(() => {
-    window.sessionStorage.removeItem('jwt')
-    setJwt(null)
-  }, [setJwt])
-
-  return { login, logout, user, isLogged: Boolean(jwt) }
+  return { user }
 }
 
 export default useUser
